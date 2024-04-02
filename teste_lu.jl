ea94e66e5e4a3798535eb934d3930fcaba8dbc71
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2024-04-02 10:18:48 -0300

    user-defined zone mapping

--- teste_lu.jl
+++ teste_lu.jl
@@ -9,5 +9,5 @@ end
 @everywhere workers() @everywhere workers() using NPBApps
 
 using NPBApps
-LU.go(LU.CLASS_A; itimer=2, npb_verbose=3)
+LU.go(LU.CLASS_A; itimer=2, npb_verbose=3, zone_mapping=[(2, [1,2,3]), (3, [4,5,6,7,8,9,10,11,12,13,14,15,16])])
 
