4f6b4d5df66261ad13df1cb88b515ccb23a3001e
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2024-04-10 08:57:01 -0300

    ...

--- src/BT/bt_data.jl
+++ src/BT/bt_data.jl
@@ -106,7 +106,7 @@ end
  function alloc_field_space(z, grid_points, x_zones, y_zones)
 
       problem_size = maximum(div.(grid_points, [x_zones, y_zones, 1]) .+ 1)
-      @info "problem_size = $problem_size"
+      @info "grid_points=$grid_points, problem_size = $problem_size"
 
       MAX_CELL_DIM[z] = div(problem_size, maxcells)+1
 
