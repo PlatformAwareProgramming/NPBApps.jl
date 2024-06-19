86057627082c2a2e39b78fdfc1be44d18e1a2486
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2024-06-19 19:53:09 -0300

    ..

--- sp.jl
+++ sp.jl
@@ -5,5 +5,5 @@ using Distributed
 addprocs(MPIWorkerManager(4); threadleve=:multiple, mpiflags=`--map-by node --hostfile /home/ubuntu/hostfile`,  exeflags=`--threads=4 --check-bounds=no --optimize=3`)
 
 @everywhere workers() using NPBApps
-@everywhere workers() SP.go(SP.CLASS_W; timers=true)
+@everywhere workers() SP.go(SP.CLASS_C; timers=true)
 
