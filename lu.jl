62a2af6b791eb4bd39ce850baa8692ea0c03bd7b
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2024-06-19 21:46:56 -0300

    ..

--- lu.jl
+++ lu.jl
@@ -2,7 +2,7 @@ using NPBApps
 using MPIClusterManagers
 using Distributed
 
-addprocs(MPIWorkerManager(4); master_tcp_interface="172.31.1.0", threadleve=:multiple, mpiflags=`--map-by node --hostfile /home/ubuntu/hostfile`,  exeflags=`--threads=4 --check-bounds=no --optimize=3`)
+addprocs(MPIWorkerManager(16); master_tcp_interface="172.31.1.0", threadleve=:multiple, mpiflags=`--map-by node --hostfile /home/ubuntu/hostfile`,  exeflags=`--threads=1 --check-bounds=no --optimize=3`)
 
 @everywhere workers() using NPBApps
 @everywhere workers() LU.go(LU.CLASS_C; timers=true)
