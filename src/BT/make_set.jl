c2c71c5aa2ca53f86ee40d8247b91ef32686d3ac
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2025-07-13 15:57:26 -0300

    ...

--- src/BT/make_set.jl
+++ src/BT/make_set.jl
@@ -94,9 +94,9 @@ function make_set(z, grid_points)
                    cell_low[z][dir, c]  = excess*(SIZE+1)+(cell_coord[z][dir, c]-excess-1)*SIZE
                    cell_high[z][dir, c] = cell_low[z][dir, c]+SIZE-1
                 end
-                  @info "cell_low[$z][$dir,$c] = $(cell_low[z][dir, c])"
-                  @info "cell_high[$z][$dir,$c] = $(cell_high[z][dir, c])"
-                  @info "cell_size[$z][$dir,$c] = $(cell_high[z][dir, c])"
+                #  @info "cell_low[$z][$dir,$c] = $(cell_low[z][dir, c])"
+                #  @info "cell_high[$z][$dir,$c] = $(cell_high[z][dir, c])"
+                #  @info "cell_size[$z][$dir,$c] = $(cell_high[z][dir, c])"
                 if cell_size[z][dir, c] <= 2
                    @error " Error: Cell size $(cell_size[z][dir, c]) too small. Min size is 3\n"
                    ierrcode = 1
